13d539e134dbb6ef89a589c75839dd51c9a9098b
Maaz <muhammed.maaz1230@gmail.com>
2026-06-19 16:57:46 +0500

    Prepare for Render deployment

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     DATABASE_DIR = os.path.join(BASE_DIR, 'data')
     DATABASE_PATH = os.path.join(DATABASE_DIR, 'university.db')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'query-optimizer-simulator-secret-key')
-    DEBUG = True
+    DEBUG = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
